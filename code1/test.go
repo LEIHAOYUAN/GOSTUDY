b10d484cc5b855ab34767f70e86c2fb73e1d59c3
haoyuan.lei <haoyuan.lei@abite.com>
2021-11-18 10:51:12 +0800

    实现简单web服务器

--- code1/test.go
+++ code1/test.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	s := "你好，世界！"
+	fmt.Fprintf(w, "%s", s)
+	log.Println(s)
+}
 
 func main() {
-	fmt.Println("hello world11111")
+	fmt.Println("服务启动.......")
+	http.HandleFunc("/", handler)
+	if err := http.ListenAndServe("localhost:1234", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
 }
