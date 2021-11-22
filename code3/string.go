b7b00a1a60a35c875f3eca0152200facb0246ec6
haoyuan.lei <haoyuan.lei@abite.com>
2021-11-22 09:42:52 +0800

    字符串截取操作测试

--- code3/string.go
+++ code3/string.go
@@ -3,10 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	s := "中国人"
-	fmt.Println(len(s))
+	s := "leihaoyuan"
+	fmt.Println("字符串长度：", len(s))
 	fmt.Println(s[0], s[1])
-	fmt.Println(s[:5])
-	fmt.Println(s[6:])
-	fmt.Println(s[:])
+	fmt.Println("字符串截取，到index = 4的位置结束：", s[:5])
+	fmt.Println("字符串截取，从index = 6的位置开始：", s[6:])
+	fmt.Println("截取全部字符串：", s[:])
 }
