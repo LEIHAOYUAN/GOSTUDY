cf2a1acd097ce405c66250cba75365b17d971839
haoyuan.lei <haoyuan.lei@abite.com>
2021-11-22 17:12:28 +0800

    slice传参如果函数内部修改了切片的长度或者容量，将会对结果产生影响

--- code9/funcParam.go
+++ code9/funcParam.go
@@ -2,20 +2,25 @@ package main
 
 import "fmt"
 
-func function(s1 []int) {
-	s1[0] += 1000
+/*
+如果函数内部改变了slice的长度或者容量，则原slice将不会发生改变
+*/
+func add1(arr []int) []int {
+	// 直接修改切片的值可以，实参也会跟着修改
+	// arr[0] = 99
+	arr = append(arr, 999)
+	fmt.Println("修改后arr：", arr)
+	return arr
+}
+
+func add2(arr []int) {
+	// 直接修改切片的值可以，实参也会跟着修改
+	arr[0] = 99
+	fmt.Println("修改后arr：", arr)
 }
 
 func main() {
-
-	var a = [5]int{1, 2, 3, 4, 5}
-
-	var s = a[:]
-
-	function(s)
-
-	fmt.Println("切片s的第0个元素：", s[0])
-
-	fmt.Println("原数组：", a)
-
+	var a = make([]int, 2, 8)
+	a = add1(a)
+	fmt.Println("修改后a：", a)
 }
