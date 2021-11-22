1d93c98359ecbd34a1041207e0356227fd936279
haoyuan.lei <haoyuan.lei@abite.com>
2021-11-22 09:46:34 +0800

    println函数多参数的使用

--- code3/string.go
+++ code3/string.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	s := "leihaoyuan"
 	fmt.Println("字符串长度：", len(s))
-	fmt.Println(s[0], s[1])
+	fmt.Println("下标index：0=", s[0], "下标index：1=", s[1])
 	fmt.Println("字符串截取，到index = 4的位置结束：", s[:5])
 	fmt.Println("字符串截取，从index = 6的位置开始：", s[6:])
 	fmt.Println("截取全部字符串：", s[:])
