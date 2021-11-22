f30ee82505caa9be2ee2088cafdc00eed3a8b619
haoyuan.lei <haoyuan.lei@abite.com>
2021-11-22 10:02:57 +0800

    修改日志描述

--- code3/string.go
+++ code3/string.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	s := "leihaoyuan"
 
-	fmt.Println("字符串长度：", len(s))
+	fmt.Println("字符串s字节数：", len(s))
 	fmt.Println("下标index：0=", s[0], "下标index：1=", s[1])
 	fmt.Println("字符串截取，到index = 4的位置结束：", s[:5])
 	fmt.Println("字符串截取，从index = 6的位置开始：", s[6:])
@@ -19,7 +19,7 @@ func main() {
 
 	fmt.Println(".............................")
 
-	fmt.Println("字符串长度：", len(s))
+	fmt.Println("字符串s字节数：", len(s))
 	fmt.Println("下标index：0=", s[0], "下标index：1=", s[1])
 	fmt.Println("字符串截取，到index = 4的位置结束：", s[:5])
 	fmt.Println("字符串截取，从index = 6的位置开始：", s[6:])
