c32d9b4c5c701cc7e58e4ee6d30732d56763c0bd
haoyuan.lei <haoyuan.lei@abite.com>
2021-11-22 09:58:49 +0800

    测试字符串的重新赋值

--- code3/string.go
+++ code3/string.go
@@ -4,9 +4,25 @@ import "fmt"
 
 func main() {
 	s := "leihaoyuan"
+
 	fmt.Println("字符串长度：", len(s))
 	fmt.Println("下标index：0=", s[0], "下标index：1=", s[1])
 	fmt.Println("字符串截取，到index = 4的位置结束：", s[:5])
 	fmt.Println("字符串截取，从index = 6的位置开始：", s[6:])
 	fmt.Println("截取全部字符串：", s[:])
+
+	fmt.Println(".............................")
+
+	var s1 = s
+	s1 = "abcdefghijklmn"
+	fmt.Println("修改后的s1=", s1)
+
+	fmt.Println(".............................")
+
+	fmt.Println("字符串长度：", len(s))
+	fmt.Println("下标index：0=", s[0], "下标index：1=", s[1])
+	fmt.Println("字符串截取，到index = 4的位置结束：", s[:5])
+	fmt.Println("字符串截取，从index = 6的位置开始：", s[6:])
+	fmt.Println("截取全部字符串：", s[:])
+
 }
