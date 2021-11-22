b649ce14506eeb77b8064bee16e027827b9934b9
haoyuan.lei <haoyuan.lei@abite.com>
2021-11-22 10:10:22 +0800

    测试字符串截取会生成新的字符串

--- code3/string.go
+++ code3/string.go
@@ -19,10 +19,15 @@ func main() {
 
 	fmt.Println(".............................")
 
-	fmt.Println("字符串s字节数：", len(s))
-	fmt.Println("下标index：0=", s[0], "下标index：1=", s[1])
-	fmt.Println("字符串截取，到index = 4的位置结束：", s[:5])
-	fmt.Println("字符串截取，从index = 6的位置开始：", s[6:])
-	fmt.Println("截取全部字符串：", s[:])
+	fmt.Println("字符串s1字节数：", len(s1), "s1的地址：", &s1)
+
+	fmt.Println("字符串s字节数：", len(s), "s的地址：", &s)
+	fmt.Println("s[0]=", s[0], "s[1]=", s[1])
+	var sub1 = s[:5]
+	fmt.Println("字符串截取，到index = 4的位置结束：", sub1, "s[:5]地址：", &sub1)
+	var sub2 = s[6:]
+	fmt.Println("字符串截取，从index = 6的位置开始：", sub2, "s[6:]地址：", &sub2)
+	var sub3 = s[:]
+	fmt.Println("截取全部字符串：", sub3, "s[:]地址：", &sub3)
 
 }
