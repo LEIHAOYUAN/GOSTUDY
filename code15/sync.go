ecf2c7fa55f89b4676edc8dcf6094234d750e320
haoyuan.lei <haoyuan.lei@abite.com>
2021-11-23 14:17:53 +0800

    waitgroup使用注意事项

--- code15/sync.go
+++ code15/sync.go
@@ -5,11 +5,19 @@ import (
 	"sync"
 )
 
+/*
+【注意】：
+当sync.WaitGroup作为参数传递到函数内且调用Done方法的时候，一定不要忘记函数的值传递特性，这里应该传递sync.WaitGroup变量的地址，
+如果传递值则会复制另一个sync.WaitGroup类型变量出来，和函数外的那个变量就没有关系了，
+即便调用了Done方法，最后还是会报DeadLock错误。
+*/
 func main() {
 	var wg sync.WaitGroup
 	for i := 0; i < 20; i++ {
+		// 给计数器+1
 		wg.Add(1)
 		go func(x int) {
+			// 给计数器-1
 			defer wg.Done()
 			fmt.Print(" ", x)
 		}(i)
