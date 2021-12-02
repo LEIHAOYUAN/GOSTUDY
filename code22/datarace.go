7dd606c69d7afbdfe57d6c3bd56502221446f276
haoyuan.lei <haoyuan.lei@abite.com>
2021-12-02 16:57:23 +0800

    代码注释

--- code22/datarace.go
+++ code22/datarace.go
@@ -1,17 +1,25 @@
 package code22
 
+/*
+Go语言的多线程模式提倡：不要通过共享内存进行通信，而是要通过通信来共享内存
+*/
 // 设置数字值
 var realNum = make(chan int)
 
 // 设置的增减额
 var delta = make(chan int)
 
+// SetNumber 将参数n写入通道realNum
 func SetNumber(n int) {
 	realNum <- n
 }
+
+// ChangeByDelta 向delta通道写入参数d的值
 func ChangeByDelta(d int) {
 	delta <- d
 }
+
+// GetNumber 从realNum通道获取值
 func GetNumber() int {
 	return <-realNum
 }
